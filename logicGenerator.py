5058e78c64f8358ad3e61bba1178c582b560d7c5
Sittivit Bunlungsak <zcabbun@ucl.ac.uk>
2023-03-23 15:06:34 +0000

    updated get_answers function

--- logicGenerator.py
+++ logicGenerator.py
@@ -3,7 +3,7 @@ from DNFGenerator import DNFGenerator
 import random
 
 def get_questions(max_variable):
-    openai.api_key = "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"
+    openai.api_key = "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"
 
     messages = [
         {"role": "system",
@@ -39,29 +39,61 @@ def replace_character(string, old, new):
             copy += i
     return copy
 
-def get_answer(question):
-    replace_operator = zip(['!', '|', '&', '=>', '<=>'], ['not ', 'or', 'and', 'or', 'or'])
+def get_answer_validation(question):
+    print(question)
+    sample = []
     DNF = DNFGenerator(question)
-    num_parameters = len(DNF.parameters)
-    expression = DNF.valid_expression[0]
-    for j, k in replace_operator:
-        expression = expression.replace(j, k)
 
-    print(expression)
+    num_parameters = len(DNF.parameters)
+    expression = DNF.generateDNF()
+
     parameters = DNF.parameters
     possible_conditions = [bin(i).replace("0b", "") for i in range(2**num_parameters)]
     possible_conditions = [(num_parameters-len(i))*'0' + i for i in possible_conditions]
-    print(possible_conditions)
     for i in possible_conditions:
         condition = [*i]
         variable_condition = zip(parameters, condition)
         temp = expression
         for i, j in variable_condition:
             temp = replace_character(temp, i, j)
-        print(temp)
-        print(eval(temp))
+        sample.append(bool(eval(temp)))
+
+
+    valid = True
+    for i in sample:
+        if i == False:
+            valid = False
+    return valid
+
+def get_answer_satisfy(question):
+    print(question)
+    sample = []
+    DNF = DNFGenerator(question)
+
+    num_parameters = len(DNF.parameters)
+    expression = DNF.generateDNF()
+
+    parameters = DNF.parameters
+    possible_conditions = [bin(i).replace("0b", "") for i in range(2**num_parameters)]
+    possible_conditions = [(num_parameters-len(i))*'0' + i for i in possible_conditions]
+    for i in possible_conditions:
+        condition = [*i]
+        variable_condition = zip(parameters, condition)
+        temp = expression
+        for i, j in variable_condition:
+            temp = replace_character(temp, i, j)
+        sample.append(bool(eval(temp)))
+
+    satisfy = False
+    for i in sample:
+        if i == True:
+            valid = True
+    return satisfy
+
 
 
 
 questions = list_questions()
-get_answer(random.choice(questions))
+question = random.choice(questions)
+print(get_answer_validation(question))
+print(get_answer_satisfy(question))
